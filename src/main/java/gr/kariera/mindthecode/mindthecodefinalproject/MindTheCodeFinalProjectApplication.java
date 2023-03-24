fa6aa01659dcb5258220491d4af3e924e011a7a0
Panagiotis Liaskos <pnliaskos@gmail.com>
2023-03-24 21:15:37 +0200

    First Commit for connection to GitHub

---
+++ src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/MindTheCodeFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.mindthecodefinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindTheCodeFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MindTheCodeFinalProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gr/kariera/mindthecode/mindthecodefinalproject/MindTheCodeFinalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.kariera.mindthecode.mindthecodefinalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindTheCodeFinalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
