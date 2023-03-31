9d6d7d3d120a718463413af96dba1fb190ee612a
asfyld <ang.loudn@gmail.com>
2023-03-31 20:46:16 +0300

    Basic sec config pt.2

--- src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/config/SecurityConfiguration.java
+++ src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/config/SecurityConfiguration.java
@@ -1,54 +1,32 @@
-package willydekeyser.config;
-
-import static org.springframework.security.config.Customizer.withDefaults;
-
-import javax.sql.DataSource;
+package gr.kariera.mindthecode.mindthecodefinalproject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-
 	@Bean
 	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-			.authorizeHttpRequests(authConfig -> {
-				authConfig.requestMatchers(HttpMethod.GET, "/").permitAll();
-				authConfig.requestMatchers(HttpMethod.GET, "/user").hasRole("USER");
-				authConfig.requestMatchers(HttpMethod.GET, "/admin").hasRole("ADMIN");
-				authConfig.anyRequest().authenticated();
-			})
-			.csrf(csrf -> csrf.disable())
-			.headers().frameOptions().disable()
-			.and()
-			.formLogin(withDefaults()) // Login with browser and Build in Form
-			.httpBasic(withDefaults()); // Login with Insomnia or Postman and Basic Auth
+				.authorizeHttpRequests(authConfig -> {
+					authConfig.requestMatchers("/").permitAll();
+					authConfig.requestMatchers("/user/**").authenticated();
+					authConfig.requestMatchers("/admin/**").denyAll();
+
+				})
+				.formLogin(Customizer.withDefaults()) // Login with browser and Form
+				.httpBasic(Customizer.withDefaults()); // Login with Insomnia and Basic Auth
 		return http.build();
 	}
-	
-	@Bean
-	JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
-		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
-		return jdbcUserDetailsManager;
-	}
-		
-	@Bean
-	DataSource dataSource() {
-		return new EmbeddedDatabaseBuilder()
-			.setType(EmbeddedDatabaseType.H2)
-			.addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
-			.build();
-	}
-	
+
 }
