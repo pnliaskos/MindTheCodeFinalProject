5f9e4b40a36ccbefd38a4b127287c1392f786357
Panagiotis Liaskos <pnliaskos@gmail.com>
2023-04-01 00:01:15 +0300

    Comments for Security

--- src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/Controllers/ProductApiController.java
+++ src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/Controllers/ProductApiController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(path = "/api")
-@CrossOrigin(origins = "http://localhost:8080")
+//@CrossOrigin(origins = "http://localhost:8080")
 public class ProductApiController {
     private final ProductService service;
 
