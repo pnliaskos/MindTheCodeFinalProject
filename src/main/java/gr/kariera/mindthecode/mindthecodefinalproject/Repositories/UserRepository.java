7c0cd480716929ebdb475b1ea4957b140e41f6f3
asfyld <ang.loudn@gmail.com>
2023-03-31 10:18:04 +0300

    Added User repo

---
+++ src/main/java/gr/kariera/mindthecode/mindthecodefinalproject/Repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package gr.kariera.mindthecode.mindthecodefinalproject.Repositories;
+
+import gr.kariera.mindthecode.mindthecodefinalproject.Entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+    Optional<UserEntity> findByUsername(String username);
+    Boolean existsByUsername(String username);
+}
